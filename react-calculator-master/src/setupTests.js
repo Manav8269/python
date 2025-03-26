98cc82444ce161264d57918f310f23b97128a9e6
Manav Gupta <140322779+Manav8269@users.noreply.github.com>
2025-03-26 12:04:50 +0530

    Update setupTests.js

--- react-calculator-master/src/setupTests.js
+++ react-calculator-master/src/setupTests.js
@@ -1,5 +1,23 @@
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
+// Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+// Mock localStorage
+beforeAll(() => {
+  global.localStorage = {
+    getItem: jest.fn(() => null),
+    setItem: jest.fn(),
+    removeItem: jest.fn(),
+    clear: jest.fn(),
+  };
+});
+
+// Mock window.alert to prevent popups in tests
+global.alert = jest.fn();
+
+// Mock Fetch API
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve({}),
+  })
+);
