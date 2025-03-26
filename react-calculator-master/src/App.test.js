8e6de52e2426be79e53ca2a9665161d7d980fa23
Manav Gupta <Guptamanav189@gmail.com>
2025-03-26 12:40:27 +0530

    commit

--- react-calculator-master/src/App.test.js
+++ react-calculator-master/src/App.test.js
@@ -1,13 +1,8 @@
-import React from 'react'; // Add this line
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders calculator header', () => {
+test("renders calculator header", () => {
   render(<App />);
   expect(screen.getByText(/Calculator/i)).toBeInTheDocument();
-});
-
-test('renders a number button', () => {
-  render(<App />);
-  expect(screen.getByText(/7/i)).toBeInTheDocument(); // Example: Check if "7" button exists
 });
