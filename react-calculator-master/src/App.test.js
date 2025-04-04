9eb4ba317198cc97ac49b02e54c6fc0fbf9acd94
Manav Gupta <140322779+Manav8269@users.noreply.github.com>
2025-04-04 10:29:52 +0530

    Delete react-calculator-master directory

--- react-calculator-master/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders calculator header", () => {
-  render(<App />);
-  expect(screen.getByText(/Calculator/i)).toBeInTheDocument();
-});
--- react-calculator-master/src/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageReporters: ["lcov", "text-summary"],
-  coveragePathIgnorePatterns: ["/node_modules/", "/test/"],
-};
